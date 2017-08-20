146e8c38d84d2fef8eea28a6caaae2427707c906
cardoso <matheusmcrds@gmail.com>
2017-08-20 11:30:10 -0300

    feat: expose TTF_Init, TTF_OpenFont, TTF_SizeText, TTF_RenderText_Solid

--- Sources/CSDLTTF/CSDLTTF.swift
+++ Sources/CSDLTTF/CSDLTTF.swift
@@ -1,3 +1,6 @@
 import CSDLTTFRaw
 
-@_exported import CSDLTTFRaw
+@_exported import func CSDLTTFRaw.TTF_Init
+@_exported import func CSDLTTFRaw.TTF_OpenFont
+@_exported import func CSDLTTFRaw.TTF_SizeText
+@_exported import func CSDLTTFRaw.TTF_RenderText_Solid
